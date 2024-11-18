e4f88d6f52c919aa8f369910ffee1445eaaa75ba
gabrielley <gabriel.ley@outlook.fr>
2024-11-18 21:43:11 +0100

    Use config file rather than env variables

--- core/orm.js
+++ core/orm.js
@@ -1,10 +1,14 @@
 const Sequelize = require('sequelize');
 
+const config = require('../config/config.json');
+
+const databaseConfig = config[process.env.NODE_ENV || 'development'];
+
 const sequelizeInstance = new Sequelize(
-    process.env.DB_BIS_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {host: 'localhost', dialect: 'mysql'}
+    databaseConfig.database,
+    databaseConfig.username,
+    databaseConfig.password,
+    {host: databaseConfig.host, dialect: 'mysql'}
 );
 
 module.exports = sequelizeInstance;
