06d7e9aecec449123b5ee09ff839740d6e6ecd64
gabrielley <gabriel.ley@outlook.fr>
2024-11-18 20:58:14 +0100

    Rename bin/www

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Hello world v2");
+  res.send("Hello world v3");
 });
 
 router.get('/favicon.ico', (req, res) => res.sendStatus(204));
