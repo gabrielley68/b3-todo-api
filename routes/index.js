9d8dc0d8305764a659ecaea203168c49cabcafd3
gabrielley <gabriel.ley@outlook.fr>
2024-11-18 22:41:06 +0100

    Break pipeline

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Hello world v3");
+  res.send("Hello world v4");
 });
 
 router.get('/favicon.ico', (req, res) => res.sendStatus(204));
