00630799ab843ad6999eff0803a48308648e2422
gabrielley <gabriel.ley@outlook.fr>
2024-11-18 23:02:17 +0100

    Try fixing config

--- core/orm.js
+++ core/orm.js
@@ -5,10 +5,10 @@ const config = require('../config/config.json');
 const databaseConfig = config[process.env.NODE_ENV || 'development'];
 
 const sequelizeInstance = new Sequelize(
-    databaseConfig.database,
-    databaseConfig.username,
-    databaseConfig.password,
-    {host: databaseConfig.host, dialect: 'mysql'}
+    process.env.DB_BIS_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {host: 'localhost', dialect: 'mysql'}
 );
 
 module.exports = sequelizeInstance;
